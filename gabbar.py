945fdf0c6b810ee463575e0eee960b2e1b1c4829
vaibhavsingh001 <singhvaibhav.singh6@gmail.com>
2016-10-05 09:50:15 +0530

    gabbar hua update
    addmi hue fractional

--- gabbar.py
+++ gabbar.py
@@ -1,9 +1,8 @@
 #likes to see basanti ka dance
 import random
-a=int(1,2,3,4,5)
+
 b=int(input("Kitne aadmi the? (no homo)"))
-random.a
+a=random.uniform(1,5)
 print("Aur tum",a,"! Phir bhi khali haath aa gaye.(still no homo)")
 print("Kya socha tha? Sarkar khush hoga? Sabashi dega?")
-
 
