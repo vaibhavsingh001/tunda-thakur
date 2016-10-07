b1bc990707ac6d792974e89ed30df16566c740eb
vaibhavsingh001 <singhvaibhav.singh6@gmail.com>
2016-10-07 10:09:33 +0530

    Add files via upload

---
+++ rapid fire round.py
@@ -0,0 +1,34 @@
+#gabbar rapid fire round
+a=int(input("holi kab hai ,kab hai holi: "))
+b=int(input("sarkar humpe kitna inaam rakhi hai: "))
+c=int(input("kitne aadami the: "))
+d=input("kiske pair chalenge aur kiske sase chalenge: ")
+e=float(input("station se ramgad ka fair kitna tha: "))
+#(kbc)
+#a=1000
+#b=2000
+#c=3000
+#d=4000
+#e=5000
+#prize money is only going to allowed if all the ans are correct.
+if a==24 :
+    if b ==5000:
+        if c==2:
+            if 'basanti'and'viru' in d :
+                if e ==2.56:
+                    print('Aap jite : Baaba ji ka thullu')
+                else:
+                    print('Sarkar khush nahi hua')
+            else:
+                print('Sarkar khush nahi hua')
+        else:
+            print('Sarkar khush nahi hua')
+    else:
+        print('Sarkar khush nahi hua')
+else:
+    print('Sarkar khush nahi hua')
+    
+
+
+
+
